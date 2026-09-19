d03124f9c07d28d8bab95ef86e66dd70739d674d
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-19 15:28:51 +0530

    Update Solution.java

--- Day08-Two-Pointers-Sliding-Window/07-Longest-Repeating-Character-Replacement/Solution.java
+++ Day08-Two-Pointers-Sliding-Window/07-Longest-Repeating-Character-Replacement/Solution.java
@@ -1,1 +1,43 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
 
+        int[] frequency = new int[26];
+
+        int left = 0;
+        int maxFrequency = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            int index = s.charAt(right) - 'A';
+
+            frequency[index]++;
+
+            maxFrequency = Math.max(
+                maxFrequency,
+                frequency[index]
+            );
+
+            int windowLength = right - left + 1;
+
+            int replacements = windowLength - maxFrequency;
+
+            while (replacements > k) {
+
+                frequency[s.charAt(left) - 'A']--;
+
+                left++;
+
+                windowLength = right - left + 1;
+                replacements = windowLength - maxFrequency;
+            }
+
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
+        }
+
+        return maxLength;
+    }
+}
