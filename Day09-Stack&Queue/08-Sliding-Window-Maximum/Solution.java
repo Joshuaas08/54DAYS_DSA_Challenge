16e65a6af4f7308405a7edf94a7a22fbd4230d3e
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-20 00:12:57 +0530

    Update Solution.java

--- Day09-Stack&Queue/08-Sliding-Window-Maximum/Solution.java
+++ Day09-Stack&Queue/08-Sliding-Window-Maximum/Solution.java
@@ -1,1 +1,40 @@
+import java.util.*;
 
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] result = new int[n - k + 1];
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int right = 0; right < n; right++) {
+
+            // Remove indices outside the window
+            while (!deque.isEmpty() &&
+                   deque.peekFirst() <= right - k) {
+
+                deque.pollFirst();
+            }
+
+            // Remove smaller elements
+            while (!deque.isEmpty() &&
+                   nums[deque.peekLast()] <= nums[right]) {
+
+                deque.pollLast();
+            }
+
+            deque.offerLast(right);
+
+            // Start recording answers
+            if (right >= k - 1) {
+
+                result[right - k + 1] =
+                    nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+}
