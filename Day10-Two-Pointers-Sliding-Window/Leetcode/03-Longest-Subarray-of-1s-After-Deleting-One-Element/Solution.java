6f2e76182e642fbe1a42c14ab8f1c17ed4db6336
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 12:29:46 +0530

    Update Solution.java

--- Day10-Two-Pointers-Sliding-Window/Leetcode/03-Longest-Subarray-of-1s-After-Deleting-One-Element/Solution.java
+++ Day10-Two-Pointers-Sliding-Window/Leetcode/03-Longest-Subarray-of-1s-After-Deleting-One-Element/Solution.java
@@ -1,1 +1,32 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
 
+        int left = 0;
+        int zeros = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            while (zeros > 1) {
+
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+
+                left++;
+            }
+
+            // Delete one element
+            maxLength = Math.max(
+                maxLength,
+                right - left
+            );
+        }
+
+        return maxLength;
+    }
+}
