355c7f330be2726625499b234862f39e4637df41
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 15:31:37 +0530

    Add lengthOfLongestSubstring method in Solution class
    Implement a method to find the length of the longest substring without repeating characters.

--- Day11-Hashing/Leetcode/05-Longest-Substring-Without-Repeating-Characters/Solution.java
+++ Day11-Hashing/Leetcode/05-Longest-Substring-Without-Repeating-Characters/Solution.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> lastSeen = new HashMap<>();
+
+        int left = 0;
+        int longest = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char current = s.charAt(right);
+
+            if (lastSeen.containsKey(current)) {
+                left = Math.max(
+                    left,
+                    lastSeen.get(current) + 1
+                );
+            }
+
+            lastSeen.put(current, right);
+
+            longest = Math.max(
+                longest,
+                right - left + 1
+            );
+        }
+
+        return longest;
+    }
+}
