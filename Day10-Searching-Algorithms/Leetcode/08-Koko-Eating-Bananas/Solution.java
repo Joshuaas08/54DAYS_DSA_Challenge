e0f264867ae41e8cba603b14619eba359e367891
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 13:36:22 +0530

    Update Solution.java

--- Day10-Searching-Algorithms/Leetcode/08-Koko-Eating-Bananas/Solution.java
+++ Day10-Searching-Algorithms/Leetcode/08-Koko-Eating-Bananas/Solution.java
@@ -1,1 +1,49 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
 
+        int left = 1;
+        int right = 0;
+
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        int answer = right;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (canFinish(piles, h, mid)) {
+
+                answer = mid;
+
+                // Try a smaller speed
+                right = mid - 1;
+
+            } else {
+
+                // Need a faster speed
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean canFinish(int[] piles, int h, int speed) {
+
+        long hours = 0;
+
+        for (int pile : piles) {
+
+            hours += (pile + speed - 1) / speed;
+
+            if (hours > h) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
