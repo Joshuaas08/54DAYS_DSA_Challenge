5a990ac5d3a6b068fcd7c31a3a3a47be7561495e
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 13:34:55 +0530

    Update Solution.java

--- Day10-Searching-Algorithms/Leetcode/07-Sqrt-X/Solution.java
+++ Day10-Searching-Algorithms/Leetcode/07-Sqrt-X/Solution.java
@@ -1,1 +1,36 @@
+class Solution {
+    public int mySqrt(int x) {
 
+        if (x < 2) {
+            return x;
+        }
+
+        long left = 1;
+        long right = x / 2;
+
+        int answer = 1;
+
+        while (left <= right) {
+
+            long mid = left + (right - left) / 2;
+
+            long square = mid * mid;
+
+            if (square == x) {
+                return (int) mid;
+            }
+
+            if (square < x) {
+
+                answer = (int) mid;
+                left = mid + 1;
+
+            } else {
+
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
