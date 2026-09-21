e00b8c6154de0433b57c319ecfe61cc1233e9397
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 12:33:34 +0530

    Add solution for longest subarray with absolute difference
    Implemented a solution to find the longest subarray with an absolute difference less than or equal to a given limit using two deques for maintaining the minimum and maximum values.

--- Day10-Two-Pointers-Sliding-Window/Leetcode/07-Longest-Subarray-with-Absolute-Difference-Less-Than-or-Equal-to-Limit/Solution.java
+++ Day10-Two-Pointers-Sliding-Window/Leetcode/07-Longest-Subarray-with-Absolute-Difference-Less-Than-or-Equal-to-Limit/Solution.java
@@ -1,1 +1,55 @@
+import java.util.*;
 
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+
+        Deque<Integer> increasing = new ArrayDeque<>();
+        Deque<Integer> decreasing = new ArrayDeque<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            // Maintain increasing deque
+            while (!increasing.isEmpty() &&
+                   nums[increasing.peekLast()] > nums[right]) {
+
+                increasing.pollLast();
+            }
+
+            increasing.offerLast(right);
+
+            // Maintain decreasing deque
+            while (!decreasing.isEmpty() &&
+                   nums[decreasing.peekLast()] < nums[right]) {
+
+                decreasing.pollLast();
+            }
+
+            decreasing.offerLast(right);
+
+            // Shrink invalid window
+            while (nums[decreasing.peekFirst()] -
+                   nums[increasing.peekFirst()] > limit) {
+
+                if (increasing.peekFirst() == left) {
+                    increasing.pollFirst();
+                }
+
+                if (decreasing.peekFirst() == left) {
+                    decreasing.pollFirst();
+                }
+
+                left++;
+            }
+
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
+        }
+
+        return maxLength;
+    }
+}
