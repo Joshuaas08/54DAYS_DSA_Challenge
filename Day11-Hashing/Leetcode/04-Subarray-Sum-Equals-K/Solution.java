2f78cf6494c070ccf48199944cec76b9be5c15c2
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-21 15:26:18 +0530

    Update Solution.java

--- Day11-Hashing/Leetcode/04-Subarray-Sum-Equals-K/Solution.java
+++ Day11-Hashing/Leetcode/04-Subarray-Sum-Equals-K/Solution.java
@@ -1,1 +1,21 @@
+from collections import defaultdict
 
+class Solution:
+    def subarraySum(self, nums, k):
+        prefix_count = defaultdict(int)
+
+        # Empty prefix
+        prefix_count[0] = 1
+
+        prefix_sum = 0
+        result = 0
+
+        for num in nums:
+            prefix_sum += num
+
+            # prefix_sum - previous_sum = k
+            result += prefix_count[prefix_sum - k]
+
+            prefix_count[prefix_sum] += 1
+
+        return result
