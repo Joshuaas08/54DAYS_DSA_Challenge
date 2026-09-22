c04dc7df07a5e61883eda722fad8064a3d99cc2a
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-22 09:16:22 +0530

    Update Solution.java

--- Day12-Sorting-Algorithms/Leetcode/03-Insertion-Sort-List/Solution.java
+++ Day12-Sorting-Algorithms/Leetcode/03-Insertion-Sort-List/Solution.java
@@ -1,1 +1,25 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
 
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+
+            ListNode previous = dummy;
+
+            while (previous.next != null &&
+                   previous.next.val < current.val) {
+                previous = previous.next;
+            }
+
+            current.next = previous.next;
+            previous.next = current;
+
+            current = next;
+        }
+
+        return dummy.next;
+    }
+}
