8d08bef1bffa9016458994a7a4a45947828df4b5
Joshuaas08 <joshuaasirsolomon@gmail.com>
2026-09-22 09:18:06 +0530

    Update Solution.java

--- Day12-Sorting-Algorithms/Leetcode/05-Sort-List/Solution.java
+++ Day12-Sorting-Algorithms/Leetcode/05-Sort-List/Solution.java
@@ -1,1 +1,54 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
 
+        // Find the middle
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Split the list
+        ListNode second = slow.next;
+        slow.next = null;
+
+        // Sort both halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(second);
+
+        // Merge sorted halves
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+
+            current = current.next;
+        }
+
+        if (left != null) {
+            current.next = left;
+        }
+
+        if (right != null) {
+            current.next = right;
+        }
+
+        return dummy.next;
+    }
+}
